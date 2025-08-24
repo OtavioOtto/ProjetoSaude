3b921651a11b1cdf92e6c8b4a9678ef3f8253705
OtavioOtto <otavio.ols@puccampinas.edu.br>
2025-08-24 18:51:33 -0300

    Teste movimentacao

---
+++ Assets/Scripts/PlayerBehaviours.cs
@@ -0,0 +1,63 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerBehaviours : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    [Header("Player Animations")]
+    private Animator anim;
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        PlayerMovement();
+        Animate();
+        if((input.x < 0 && !facingLeft) || (input.x  > 0 && facingLeft))
+            Flip();
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = input * speed;
+    }
+
+    void PlayerMovement() 
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && (input.x != 0 || input.y != 0))
+        {
+            lastMoveDirection = input;
+        }
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input.Normalize();
+    }
+
+    void Animate() 
+    {
+        anim.SetFloat("MoveX", input.x);
+        anim.SetFloat("MoveY", input.y);
+        anim.SetFloat("MoveMagnitude", input.magnitude);
+        anim.SetFloat("LastMoveX", lastMoveDirection.x);
+        anim.SetFloat("LastMoveY", lastMoveDirection.y);
+    }
+
+    void Flip() 
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingLeft = !facingLeft;
+    }
+}
